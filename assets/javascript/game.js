7e47c7d6ef421a3fad7eea4abbb2861f52c9c7e0
tyfal <tfalcoff@gmail.com>
2019-06-23 21:35:43 -0500

    organising the code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,48 +1,52 @@
 
 class word {
+
     constructor(val) {
         this.name = val.toLowerCase();
         this.arr = this.name.split("");
         this.progress = Array(this.arr.length).fill("_");
         this.badGuesses = [];
         this.wrongCount = 0;
-        this.docProgress = function () {
-            document.getElementById("progress").innerHTML = this.progress;
-        }
-        this.docBadGuesses = function () {
-            document.getElementById("badGuesses").innerHTML = this.badGuesses;
-        }
-        this.guessLetter = function (letter) {
-            if (isLetter(letter) && letter.length===1) {
-                var guess = letter.toLowerCase();
-                var match = false;
-                for (var i = 0; i < this.arr.length; i++) {
-                    if (this.arr[i] === letter) {
-                        match = true;
-                        this.progress[i] = guess;
-                    }
+    }
+
+    docProgress() {
+        document.getElementById("progress").innerHTML = this.progress;
+    }
+
+    docBadGuesses() {
+        document.getElementById("badGuesses").innerHTML = this.badGuesses;
+    }
+
+    isLetter(val) {
+        var letters = /^[A-Za-z]+$/;
+        return val.match(letters);
+    }
+
+    guessLetter(letter) {
+        if (this.isLetter(letter) && letter.length === 1) {
+            var guess = letter.toLowerCase();
+            var match = false;
+            for (var i = 0; i < this.arr.length; i++) {
+                if (this.arr[i] === letter) {
+                    match = true;
+                    this.progress[i] = guess;
                 }
-                if (match) {
-                    this.docProgress();
-                    if (this.progress.indexOf("_") === -1) {
-                        document.getElementById("instruction").innerHTML = "You Won!!!";
-                    }
-                } else {
-                    this.badGuesses.push(letter);
-                    this.wrongCount++;
-                    this.docBadGuesses();
-                    if (this.wrongCount===10) {
-                        document.getElementById("instruction").innerHTML = "You Lost :'(";
-                    }
+            }
+            if (match) {
+                this.docProgress();
+                if (this.progress.indexOf("_") === -1) {
+                    document.getElementById("instruction").innerHTML = "You Won!!!";
+                }
+            } else {
+                this.badGuesses.push(letter);
+                this.wrongCount++;
+                this.docBadGuesses();
+                if (this.wrongCount === 10) {
+                    document.getElementById("instruction").innerHTML = "You Lost :'(";
                 }
             }
         }
     }
-}
-
-function isLetter(val) {
-    var letters = /^[A-Za-z]+$/;
-    return val.match(letters);
 }
 
 function game() {
@@ -52,6 +56,6 @@ function game() {
     document.getElementById("instruction").innerHTML = "Key your guesses at will, but be wise...";
     document.onkeyup = function (event) {
         var letter = event.key;
-        guessMe.guessLetter(letter);
+        guessMe.guessLetter(letter.toLowerCase());
     }
 }
